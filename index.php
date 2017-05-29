c103bcc7e195f7764686bac9edc937ea94f2f929
faizanahmed963 <faizanahmed963@yahoo.com>
2017-05-29 16:46:29 +0500

    JavaScript + JQuery.
    Addin bootstarp javascript and jquery.

--- index.php
+++ index.php
@@ -54,6 +54,17 @@
         <!-- /.container -->
     </nav>
 
+	
+	
+	
+	<!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+	
+	
+	
 </body>
 
 </html>
