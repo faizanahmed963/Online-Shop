07293e546f9333388ff81a4f0e406b9e28a95346
faizanahmed963 <faizanahmed963@yahoo.com>
2017-05-31 00:29:34 +0500

    Trying TedX website head

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
 
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,6 +17,11 @@
 	
 	<!-- Custom CSS -->
     <link href="css/shop-homepage.css" rel="stylesheet">
+	<link href="css/header.css" rel="stylesheet">
+	
+ 
+ 
+ 
 	
 </head>
 
@@ -23,6 +29,8 @@
 <body>
 
     <!-- Navigation -->
+	
+	<div>
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
@@ -33,7 +41,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">Online Shopping</a>
+                <a class="navbar-brand" href="/">Online Shopping</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -48,14 +56,97 @@
                         <a href="#">Contact</a>
                     </li>
                 </ul>
+				<ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
             </div>
             <!-- /.navbar-collapse -->
         </div>
         <!-- /.container -->
     </nav>
 
+	</div>
 	
 	
+	
+	
+	<!-- Header -->
+	<div>
+	<header class="header" id="header" role="banner">
+	
+	<div class="header-top-wrapper">
+      <div class="header-top-wrapper-inner">
+          <div class="header__region region region-header">
+    <div id="block-cd-social-block-social-links-round" class="block block-cd-social-block first last odd">
+  <div class="block-interior">
+            
+    <div class="links-wrapper">
+            <div class="link-wrapper link-type-twitter">
+        <span class="link">
+          <a target="_blank" href="https://twitter.com/TEDxPerth"><span>Twitter</span></a>
+        </span>
+      </div>
+                <div class="link-wrapper link-type-youtube">
+        <span class="link">
+          <a target="_blank" href="https://www.youtube.com/user/tedxperth"><span>YouTube</span></a>
+        </span>
+      </div>
+                <div class="link-wrapper link-type-facebook">
+        <span class="link">
+          <a target="_blank" href="https://www.facebook.com/TEDxPerth"><span>Facebook</span></a>
+        </span>
+      </div>
+                <div class="link-wrapper link-type-gplus">
+        <span class="link">
+          <a target="_blank" href="https://plus.google.com/+Tedxperth"><span>Google+</span></a>
+        </span>
+      </div>
+                      <div class="link-wrapper link-type-linkedin">
+        <span class="link">
+          <a target="_blank" href="https://www.linkedin.com/company/tedxperth"><span>Linkedin</span></a>
+        </span>
+      </div>
+      </div>
+  </div>
+</div>
+  </div>
+        <a href="/" title="Home"
+           rel="home" class="header__logo" id="logo"><img
+            src="http://www.tedxperth.org/sites/default/themes/tedx/logo.png" alt="Home"
+            class="header__logo-image"/></a>
+      </div>
+    </div>
+    <div class="header-bottom-wrapper">
+        <div class="region region-navigation">
+    <div id="block-menu-block-1" class="block block-menu-block first last odd" role="navigation">
+  <div class="block-interior">
+              <h2 class="block__title block-title">Main Menu</h2>
+        
+    <div class="menu-block-wrapper menu-block-1 menu-name-main-menu parent-mlid-0 menu-level-1">
+  <ul class="menu"><li class="menu__item is-active is-leaf first leaf active menu-mlid-214"><a href="/" class="menu__link active"><span class="link-text">Home</span></a></li>
+<li class="menu__item is-leaf leaf menu-mlid-934"><a href="/news" class="menu__link"><span class="link-text">News</span></a></li>
+<li class="menu__item is-leaf leaf menu-mlid-907"><a href="/talks" class="menu__link"><span class="link-text">Watch Talks</span></a></li>
+<li class="menu__item is-expanded expanded menu-mlid-946"><a href="http://www.tedxperth.org/speakers-and-performers" class="menu__link"><span class="link-text">Past Events</span></a><ul class="menu"><li class="menu__item is-leaf first leaf menu-mlid-1052"><a href="/speakers-and-performers" class="menu__link"><span class="link-text">TEDxPerth 2016</span></a></li>
+<li class="menu__item is-leaf leaf menu-mlid-1048"><a href="/tedxperth-2015-0" class="menu__link"><span class="link-text">TEDxPerth 2015</span></a></li>
+<li class="menu__item is-parent is-leaf leaf has-children menu-mlid-912"><a href="/tedxperth-2014" class="menu__link"><span class="link-text">TEDxPerth 2014</span></a></li>
+<li class="menu__item is-leaf leaf menu-mlid-947"><a href="/tedxperth-2013" class="menu__link"><span class="link-text">TEDxPerth 2013</span></a></li>
+<li class="menu__item is-leaf last leaf menu-mlid-1036"><a href="/tedxperth-2012" class="menu__link"><span class="link-text">TEDxPerth 2012</span></a></li>
+</ul></li>
+<li class="menu__item is-leaf leaf menu-mlid-1051"><a href="/tedxperth-2016-partners" class="menu__link"><span class="link-text">Partners</span></a></li>
+<li class="menu__item is-expanded last expanded menu-mlid-395"><a href="/what-ted-and-tedxperth" class="menu__link"><span class="link-text">About</span></a><ul class="menu"><li class="menu__item is-leaf first leaf menu-mlid-978"><a href="/what-ted-and-tedxperth" class="menu__link"><span class="link-text">What is TED & TEDxPerth?</span></a></li>
+<li class="menu__item is-leaf leaf menu-mlid-950"><a href="/team" class="menu__link"><span class="link-text">Team</span></a></li>
+<li class="menu__item is-leaf leaf menu-mlid-1037"><a href="/contact-us" class="menu__link"><span class="link-text">Contact Us</span></a></li>
+<li class="menu__item is-leaf last leaf menu-mlid-1038"><a href="/press-requests" class="menu__link"><span class="link-text">Press Requests</span></a></li>
+</ul></li>
+</ul></div>
+  </div>
+</div>
+  </div>
+    </div>
+  </header>
+  
+  </div>
 	
 	<!-- jQuery -->
     <script src="js/jquery.js"></script>
